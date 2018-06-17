21949441ef70d53224661e5632e0b3a7b492bbb5
Florian Heck <florian.heck@hotmail.de>
2018-06-17 04:00:15 +0200

    Improve ExampleBot and adapt changes in BaseBot.

--- src/main/java/de/uni_passau/fim/sommercamp/sc2/BaseBot.java
+++ src/main/java/de/uni_passau/fim/sommercamp/sc2/BaseBot.java
@@ -1,77 +1,116 @@
 package de.uni_passau.fim.sommercamp.sc2;
 
 import com.github.ocraft.s2client.api.S2Client;
-import com.github.ocraft.s2client.protocol.game.Race;
 import com.github.ocraft.s2client.protocol.observation.Observation;
+import com.github.ocraft.s2client.protocol.request.Request;
 import com.github.ocraft.s2client.protocol.request.RequestGameInfo;
 import com.github.ocraft.s2client.protocol.request.RequestStep;
 import com.github.ocraft.s2client.protocol.response.*;
 import com.github.ocraft.s2client.protocol.spatial.Point2d;
+import com.github.ocraft.s2client.protocol.syntax.action.raw.ActionRawUnitCommandBuilder;
 import com.github.ocraft.s2client.protocol.unit.Tag;
 import com.github.ocraft.s2client.protocol.unit.Unit;
 
+import java.util.Optional;
+
 import static com.github.ocraft.s2client.protocol.action.Action.action;
+import static com.github.ocraft.s2client.protocol.action.ActionResult.SUCCESS;
 import static com.github.ocraft.s2client.protocol.action.raw.ActionRawUnitCommand.unitCommand;
 import static com.github.ocraft.s2client.protocol.data.Abilities.*;
+import static com.github.ocraft.s2client.protocol.game.GameStatus.ENDED;
+import static com.github.ocraft.s2client.protocol.game.Race.TERRAN;
 import static com.github.ocraft.s2client.protocol.request.RequestAction.actions;
 import static com.github.ocraft.s2client.protocol.request.RequestJoinGame.joinGame;
 import static com.github.ocraft.s2client.protocol.request.RequestObservation.observation;
+import static com.github.ocraft.s2client.protocol.unit.Alliance.ENEMY;
+import static com.github.ocraft.s2client.protocol.unit.Alliance.SELF;
 
 public abstract class BaseBot {
 
-    private final Race race;
     private S2Client client;
     private GameInfo info;
     private Observation observation;
+    private boolean lastActionSuccessful;
 
-    public BaseBot(S2Client client, Race race) {
-        this.race = race;
+    public BaseBot(S2Client client) {
         this.client = client;
     }
 
     public final void handle(Response response) {
-        response.as(ResponseCreateGame.class).ifPresent(r -> client.request(joinGame().as(race)));
+        response.as(ResponseCreateGame.class).ifPresent(r -> client.request(joinGame().as(TERRAN)));
         response.as(ResponseJoinGame.class).ifPresent(r -> client.request(RequestGameInfo.gameInfo()));
+        response.as(ResponseStep.class).ifPresent(r -> client.request(observation()));
+        response.as(ResponseAction.class).ifPresent(r -> lastActionSuccessful = r.getResults().stream().allMatch(ar -> ar == SUCCESS));
         response.as(ResponseGameInfo.class).ifPresent(r -> {
             info = new GameInfo(r.getPlayersInfo(), r.getMapName(), r.getStartRaw().orElse(null));
             client.request(observation());
         });
 
-        response.as(ResponseStep.class).ifPresent(r  -> client.request(observation()));
-
         // main game loop
         response.as(ResponseObservation.class).ifPresent(r -> {
             observation = r.getObservation();
-            onStep();
-            client.request(RequestStep.nextStep());
+            if (r.getStatus() != ENDED) {
+                onStep();
+                client.request(RequestStep.nextStep());
+//            } else {
+//                // Disabled to allow manual replays
+//                client.request(leaveGame());
+            }
         });
-
-        System.err.println(response.getType());
     }
 
     protected abstract void onStep();
 
-    protected void moveUnits(Unit[] units, Point2d target) {
-        client.request(actions().of(action().raw(unitCommand().forUnits(units).useAbility(MOVE).target(target))));
+    protected void moveUnits(Point2d target, Unit... units) {
+        if (units.length != 0) {
+            ActionRawUnitCommandBuilder action = unitCommand().forUnits(units).useAbility(MOVE).target(target);
+            client.request(actions().of(action().raw(action)));
+        }
     }
 
-    protected void attackTarget(Unit[] units, Tag target) {
-        client.request(actions().of(action().raw(unitCommand().forUnits(units).useAbility(ATTACK).target(target))));
+    protected void attackTarget(Tag target, Unit... units) {
+        if (units.length != 0) {
+            client.request(actions().of(action().raw(unitCommand().forUnits(units).useAbility(ATTACK).target(target))));
+        }
     }
 
-    protected void stopUnits(Unit[] units) {
-        client.request(actions().of(action().raw(unitCommand().forUnits(units).useAbility(STOP).build())));
+    protected void stopUnits(Unit... units) {
+        if (units.length != 0) {
+            client.request(actions().of(action().raw(unitCommand().forUnits(units).useAbility(STOP).build())));
+        }
+    }
+
+    protected void sendRawCommand(Request action) {
+        client.request(action);
+    }
+
+    protected Unit[] getUnits() {
+        return observation.getRaw().get().getUnits().stream().filter(u -> u.getAlliance() == SELF).toArray(Unit[]::new);
     }
 
     protected Unit[] getIdleUnits() {
-        return observation.getRaw().get().getUnits().stream().filter(u -> u.getOrders().isEmpty()).toArray(Unit[]::new);
+        return observation.getRaw().get().getUnits().stream().filter(u -> u.getAlliance() == SELF)
+                .filter(u -> u.getOrders().isEmpty()).toArray(Unit[]::new);
     }
 
-    public Race getRace() {
-        return race;
+    protected Unit[] getVisibleEnemies() {
+        return observation.getRaw().get().getUnits().stream().filter(u -> u.getAlliance() == ENEMY).toArray(Unit[]::new);
     }
+
+    protected Optional<Unit> findByTag(Tag tag) {
+        return getObservation().getRaw().get().getUnits().stream().filter(u -> u.getTag().equals(tag)).findFirst();
+    }
+
 
     protected Observation getObservation() {
         return observation;
+    }
+
+    protected GameInfo getInfo() {
+        return info;
+    }
+
+    public boolean wasLastActionSuccessful() {
+        return lastActionSuccessful;
     }
 }
--- src/main/java/de/uni_passau/fim/sommercamp/sc2/bots/ExampleBot.java
+++ src/main/java/de/uni_passau/fim/sommercamp/sc2/bots/ExampleBot.java
@@ -1,19 +1,131 @@
 package de.uni_passau.fim.sommercamp.sc2.bots;
 
 import com.github.ocraft.s2client.api.S2Client;
+import com.github.ocraft.s2client.protocol.spatial.Point;
 import com.github.ocraft.s2client.protocol.spatial.Point2d;
+import com.github.ocraft.s2client.protocol.unit.Tag;
+import com.github.ocraft.s2client.protocol.unit.Unit;
 import de.uni_passau.fim.sommercamp.sc2.BaseBot;
 
-import static com.github.ocraft.s2client.protocol.game.Race.TERRAN;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Optional;
 
 public class ExampleBot extends BaseBot {
 
+    private boolean searching = false;
+    private boolean attacking = false;
+    private boolean needCooling = false;
+
+    private Tag runnerTag;
+    private Tag targetTag;
+    private int leg = 0;
+
     public ExampleBot(S2Client client) {
-        super(client, TERRAN);
+        super(client);
     }
 
     @Override
     protected void onStep() {
-        moveUnits(getIdleUnits(), Point2d.of(100, 100));
+        if (!searching & !attacking) {
+            searching = true;
+
+            Unit runner = getUnits()[0];
+            runnerTag = runner.getTag();
+            Point base = runner.getPosition();
+            if (base.getX() > getInfo().mapData.getMapSize().getX() / 2) {
+                if (base.getY() > getInfo().mapData.getMapSize().getY() / 2) {
+                    leg = 1;
+                } else {
+                    leg = 2;
+                }
+            } else {
+                if (base.getY() > getInfo().mapData.getMapSize().getY() / 2) {
+                    leg = 0;
+                } else {
+                    leg = 3;
+                }
+            }
+        }
+
+        if (searching) {
+            patrol();
+        }
+
+        if (!attacking && getVisibleEnemies().length != 0) {
+            attacking = true;
+            searching = false;
+        }
+
+        if (attacking) {
+            attack();
+        }
+    }
+
+    private void attack() {
+        Optional<Unit> runnerOpt = findByTag(runnerTag);
+        if (!runnerOpt.isPresent()) {
+            return;
+        }
+        Unit runner = runnerOpt.get();
+
+        if (runner.getWeaponCooldown().orElse(0f) > 0f) {
+            Optional<Unit> target = findByTag(targetTag);
+            if (needCooling) {
+                target.ifPresent(t -> moveUnits(retreatPoint(runner.getPosition(), t.getPosition()), runner));
+                needCooling = false;
+            } else if (!target.isPresent()) {
+                // target down or out of sight
+                attacking = false;
+                searching = true;
+            }
+        } else if (getVisibleEnemies().length != 0) {
+            targetTag = Arrays.stream(getVisibleEnemies()).min(Comparator.comparing(e -> e.getHealth().orElse(1f))).get().getTag();
+            attackTarget(targetTag, runner);
+            needCooling = true;
+        }
+    }
+
+    private void patrol() {
+        Optional<Unit> runnerOpt = findByTag(runnerTag);
+        if (!runnerOpt.isPresent() || !runnerOpt.get().getOrders().isEmpty()) {
+            return;
+        }
+        Unit runner = runnerOpt.get();
+
+        int border = 4;
+        int maxX = getInfo().mapData.getMapSize().getX() - border;
+        int minX = border;
+        int maxY = getInfo().mapData.getMapSize().getY() - border;
+        int minY = border;
+
+        switch (leg) {
+            case 0:
+                moveUnits(Point2d.of(maxX, maxY), runner);
+                break;
+
+            case 1:
+                moveUnits(Point2d.of(maxX, minY), runner);
+                break;
+
+            case 2:
+                moveUnits(Point2d.of(minX, minY), runner);
+                break;
+
+            case 3:
+                moveUnits(Point2d.of(minX, maxY), runner);
+                break;
+        }
+
+        leg = ++leg % 4;
+    }
+
+    private Point2d retreatPoint(Point self, Point enemy) {
+        float xDist = (enemy.getX() - self.getX());
+        float yDist = (enemy.getY() - self.getY());
+        float mag = (float) Math.sqrt(xDist * xDist + yDist * yDist);
+        float x = self.getX() - xDist / mag * 5;
+        float y = self.getY() - yDist / mag * 5;
+        return Point2d.of(x, y);
     }
 }
